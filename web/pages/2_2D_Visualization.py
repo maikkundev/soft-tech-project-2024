987a0a8b114767bb582b4631a55b93aec5d58b3f
Michael Avagianos <94022525+maikkundev@users.noreply.github.com>
2024-05-04 17:04:22 +0300

    feat: add file upload

---
+++ web/pages/2_2D_Visualization.py
@@ -0,0 +1,29 @@
+import streamlit as sl
+import pandas as pd
+import io
+
+
+def read_file(file, file_extension):
+    if file_extension == "csv":
+        delim = sl.selectbox(
+            'Please enter the delimiter of the csv file. It must be either ";" or ",": ',
+            [",", ";"],
+        )
+        return pd.read_csv(
+            io.StringIO(file.getvalue().decode("utf-8")), delimiter=delim
+        )
+    else:
+        return pd.read_excel(io.BytesIO(file))
+
+
+uploaded_file = sl.file_uploader("Import a csv or an excel file:", type=["csv", "xlsx"])
+
+if uploaded_file is not None:
+    file_details = {
+        "FileName": uploaded_file.name,
+        "FileType": uploaded_file.type,
+        "FileSize": uploaded_file.size,
+    }
+    sl.write(file_details)
+    data = read_file(uploaded_file, uploaded_file.type.split("/")[-1])
+    sl.dataframe(data)
