d06bff24378d26e6bbef7da742d8fd984b209885
Lost <pandagaminggr@gmail.com>
2024-05-24 14:39:07 +0300

    chore: remove HEAD and MAIN merge tags

--- web/read_csv.py
+++ web/read_csv.py
@@ -2,22 +2,9 @@
 import streamlit as sl
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-import io
 
 
 @sl.cache_data()
-<<<<<<< HEAD
-def read_file(file, file_extension, delim):
-    if file_extension == "csv":
-        return pd.read_csv(
-            io.StringIO(file.getvalue().decode("utf-8")), delimiter=delim
-        )
-    else:
-        return pd.read_excel(io.BytesIO(file))
-
-
-def upload_and_read_file():
-=======
 # Funtion to read the file based on its extension.
 def read_file(filename, file_extension):
     if file_extension == "csv":
@@ -37,27 +24,11 @@ def read_file(filename, file_extension):
 
 def upload_and_read_file():
 
->>>>>>> main
     uploaded_file = sl.file_uploader(
         "Import a csv or an excel file:", type=["csv", "xlsx"]
     )
 
     if uploaded_file is not None:
-<<<<<<< HEAD
-        delim = sl.selectbox(
-            'Please enter the delimiter of the csv file. It must be either ";" or ",": ',
-            [",", ";"],
-        )
-        data = read_file(
-            uploaded_file,
-            uploaded_file.type.split("/")[-1],
-            delim,
-        )
-        le = LabelEncoder()
-        data["Target"] = le.fit_transform(data["Target"])
-
-        return data
-=======
         data = read_file(
             uploaded_file,
             uploaded_file.type.split("/")[-1],
@@ -85,4 +56,3 @@ def upload_and_read_file():
 
     # Return empty DataFrame and list if no file is uploaded or target is not specified
     return pd.DataFrame(), []
->>>>>>> main
